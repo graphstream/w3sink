61fecf9dabf374b1fa6f955efe9ff7479d053b95
Yoann Pigné <yoann.pigne@gmail.com>
2014-04-14 18:47:21 +0200

    FileSourceDGS with ES6 promises
    The FileSourceDGS class extends the FileSource generic class. It handles
    the asynchroneous file loading with a Promise. Promises are ES6
    standards. So the library now depends on a shime.
    
    The New way to deal with FileSourceDgs is as follows:
    
    ```javascript
    var graph = new GS.Graph('#canvas', 'svg');
    var source = new GS.FileSourceDGS();
    source.addSink(graph);
    var promise = source.begin('../../test/graph.dgs');
    promise.then(function(source){
    	// the file has been read. Now we can parse it.
    	while(source.nextStep()){
    		//read the dgs file, step by step
    	}
    } , function(err){
    	throw err
    });
    ```

--- test/test-dgs.js
+++ test/test-dgs.js
@@ -6,16 +6,25 @@
     exports.asyncTest('asynchronous test: Test graph loading', function() {
         exports.expect(1);
 
-        function after_graph_loaded() {
+        function after_graph_loaded(source) {
             exports.ok(true, 'DGS didn\'t load...');
+
+            var nbSteps = 0;
+            while(source.nextStep()){
+              nbSteps++;
+            }
+
+            exports.test("Test number of steps", function(){
+              exports.ok(nbSteps === 6, 'Problem with the number of steps');
+            });
 
             exports.test("Test nodes and edges creation/deletion", function() {
                 exports.ok(graph.nodes['1'].id === '1', 'Node ids are not ok');
-                exports.ok(graph.nodesCount === graph.nodesCount, 'Problem with the number of nodes');
-                exports.ok(graph.edgesCount === graph.edgesCount, 'Problem with the number of edges');
+                exports.ok(graph.nodesCount === 4, 'Problem with the number of nodes');
+                exports.ok(graph.edgesCount === 3, 'Problem with the number of edges');
                 exports.ok(graph.nodesCount <= 4, 'Problem with the number of nodes. Maybe something with node deletion');
                 exports.ok(graph.nodesCount >= 4, 'Problem with the number of nodes. Maybe something with node addition');
-                exports.ok(graph.edgesCount >= 3, 'Problem with the number of nodes. Maybe something with edge addition');
+                exports.ok(graph.edgesCount <= 3, 'Problem with the number of nodes. Maybe something with edge deletion');
                 exports.ok(graph.edgesCount >= 3, 'Problem with the number of nodes. Maybe something with edge addition');
             });
 
@@ -27,6 +36,7 @@
                 exports.ok(graph.nodes.n1.getAttribute('attribute2') === -12.34e7, 'Wrong value for attribute (scientific notation number).');
                 exports.ok(graph.nodes['1'].hasAttribute('attribute2') === false, 'Problem with attributes deletion');
             });
+
             exports.test("Test elaborate-typed attributes", function() {
                 exports.ok(graph.edges['2-0'].attributes.color === '#Fea453', 'Problem with color-types attributes');
                 var arr = graph.edges['0-1'].attributes.array;
@@ -47,12 +57,16 @@
                 exports.ok(map.key1 === 'val1' && map.key2[0] === 'val' && map.key2[1] === '2', 'Problems with the values of a map-typed attribute');
             });
 
-
             exports.start(); // needed to resume qunit after the async call
         }
 
-        var graph = new exports.GS.Graph('#canvas', 'svg');
+        var graph = new GS.Graph('#canvas', 'svg');
+        var source = new GS.FileSourceDGS();
+        source.addSink(graph);
 
-        graph.dgs('graph.dgs', after_graph_loaded);
+        var promise = source.begin('graph.dgs');
+        promise.then(after_graph_loaded, function(err){
+          exports.ok(false, 'promise didn\'t resolve. DGS loading failed.');
+        });
     });
 })(this);
